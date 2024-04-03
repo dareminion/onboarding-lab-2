0e0fe6ce7625bc5a6bf51386e5643bcbaa6b1577
Darren <dk4461@nyu.edu>
2024-04-02 22:00:14 -0400

    finished_lab2

--- rtl/Exercise3.sv
+++ rtl/Exercise3.sv
@@ -6,5 +6,11 @@ module Exercise3 (
     input [15:0] c,
     output [15:0] out
 );
+wire [7:0] d;
+wire [7:0] e;
+Mystery1 alpha(a [1:0], b [7:0], c [7:0], d);
+Mystery1 beta(a [3:2], b [15:8], c[15:8], e);
+Mystery2 gamma(clk, nReset, d , e , out);
+
 
 endmodule
--- rtl/Exercise4.sv
+++ rtl/Exercise4.sv
@@ -21,4 +21,17 @@ module Exercise4 (
     output logic [7:0] out
 );
 
+  always_comb
+    begin
+      if (cs == 0)
+      out = 0;
+      else begin
+      case(sel)
+      0:out = alpha;
+      1:out = beta;
+      2:out = gamma;
+      default:out = 0;
+      endcase
+      end
+    end
 endmodule
